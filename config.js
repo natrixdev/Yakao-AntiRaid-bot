20ae84550f640ea807648569a74f1527f2bc5ccc
natrix_dev <discordlenko@gmail.com>
2023-02-24 18:40:25 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: '',
+        playing: '',
+        funny: '',
+        support: "",
+        color: '#FF0000',
+        footer: ''
+    }
+}
